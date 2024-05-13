b9c30e4325eb38cd6d83eab785fba90027606e4b
BonBon <per_cherry@163.com>
2024-05-13 17:17:54 +0800

    update netlify config

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "@solidjs/start/config";
 import devtools from "solid-devtools/vite";
-import netlify from "solid-start-netlify";
-import solid from "solid-start/vite";
 
 export default defineConfig({
+  server: {
+    preset: "netlify",
+  },
   vite: {
     plugins: [
       devtools({
         autoname: true, // e.g. enable autoname
-      }),
-      solid({
-        adapter: netlify({ edge: true }),
       }),
     ],
   },
