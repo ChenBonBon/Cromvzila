7510c078bc7ef1c2fbf2d7e84b0836096ff4a35f
BonBon <per_cherry@163.com>
2024-05-15 12:04:30 +0800

    update home style

--- src/components/home/backend.tsx
+++ src/components/home/backend.tsx
@@ -8,7 +8,7 @@ export default function Backend(props: BackendProps) {
   const { children } = props;
 
   return (
-    <div class="relative overflow-hidden">
+    <div class="relative overflow-hidden w-full h-full">
       <img src="/images/home-backend.png" alt="/images/home-backend.png" />
       {children}
     </div>
